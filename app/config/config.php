ef46f134595954be15907d6572bbede1d4b8ad8c
Andres Felipe <andrespipe021028@gmail.com>
2020-08-26 20:57:49 -0500

    Repair time zone horarie :v

--- app/config/config.php
+++ app/config/config.php
@@ -5,7 +5,7 @@
 define("DB_HOST", "localhost");
 define("DB_NAME", "redsocial");
 define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "12345");
 
 // definicion de contantes del proyecto
 define("URL_APP", dirname(__DIR__));
--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 // llamando al iniciador
 require_once "../app/initializer.php";
-
+date_default_timezone_set("America/Bogota");
 // iniciamos el core
 new Core();
